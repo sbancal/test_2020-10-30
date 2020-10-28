53835a762885666e0f4b931df38257dcebba9cc0
Samuel Bancal <Samuel.Bancal@epfl.ch>
2020-10-28 22:40:50 +0100

    Created pandas.DataFrame from data
    Execution Output :
    
    humidity_dataframe:
                         1: 1m/30cm [kPa]  2: 1cm/30cm [kPa]  3: 1m/30cm [kPa]
    2020-06-02 16:48:28              10.0               12.0               8.0
    2020-06-02 17:48:34               9.0               11.0               8.0
    2020-06-02 18:48:40               6.0               11.0               8.0
    2020-06-02 19:48:46               7.0               11.0               7.0
    2020-06-02 20:48:53               6.0               11.0               7.0
    ...                               ...                ...               ...
    2020-08-29 13:22:52             200.0               11.0             200.0
    2020-08-29 14:22:58             200.0               12.0             200.0
    2020-08-29 15:23:04             200.0               12.0             200.0
    2020-08-29 16:23:10             200.0               12.0             200.0
    2020-08-29 17:23:16             200.0               13.0             200.0
    
    [1999 rows x 3 columns]
    
    humidity_dataframe.index:
    DatetimeIndex(['2020-06-02 16:48:28', '2020-06-02 17:48:34',
                   '2020-06-02 18:48:40', '2020-06-02 19:48:46',
                   '2020-06-02 20:48:53', '2020-06-02 21:48:59',
                   '2020-06-02 23:49:11', '2020-06-03 00:49:17',
                   '2020-06-03 01:49:23', '2020-06-03 02:49:30',
                   ...
                   '2020-08-29 07:22:15', '2020-08-29 08:22:21',
                   '2020-08-29 09:22:27', '2020-08-29 10:22:33',
                   '2020-08-29 11:22:39', '2020-08-29 13:22:52',
                   '2020-08-29 14:22:58', '2020-08-29 15:23:04',
                   '2020-08-29 16:23:10', '2020-08-29 17:23:16'],
                  dtype='datetime64[ns]', length=1999, freq=None)
    
    humidity_dataframe.describe():
           1: 1m/30cm [kPa]  2: 1cm/30cm [kPa]  3: 1m/30cm [kPa]
    count       1999.000000        1999.000000       1999.000000
    mean          64.572586          46.989795         53.060330
    std           66.272302          40.219993         52.094793
    min            0.100000           0.100000          0.100000
    25%            9.000000          15.000000         10.000000
    50%           35.000000          34.000000         34.000000
    75%          112.500000          69.000000         79.000000
    max          200.000000         200.000000        200.000000
    
    humidity_dataframe.dtypes:
    1: 1m/30cm [kPa]     float64
    2: 1cm/30cm [kPa]    float64
    3: 1m/30cm [kPa]     float64
    dtype: object

--- process_irrigation.py
+++ process_irrigation.py
@@ -13,12 +13,44 @@ Finaly we plot it in separated files (one per month) :
 '''
 
 import json
-import pprint
+import pandas as pd
 
 SOURCE_FILE = 'eco-sensors_irrigation_2020-06-01_2020-08-31.json'
 
+
 if __name__ == '__main__':
+    # Read JSON File
     with open(SOURCE_FILE, 'r') as f:
         data_from_json = json.load(f)
 
-    pprint.pprint(data_from_json)
+    # Read Labels
+    labels = [
+        data_from_json[0]['datasets']['label'],
+        data_from_json[1]['datasets']['label'],
+        data_from_json[2]['datasets']['label'],
+    ]
+
+    # Prepare pd.DataFrame object
+    humidity_dataframe = pd.DataFrame(
+        data={
+            labels[0]: data_from_json[0]['datasets']['data'],
+            labels[1]: data_from_json[1]['datasets']['data'],
+            labels[2]: data_from_json[2]['datasets']['data'],
+        },
+        index=data_from_json[0]['labels'],
+        dtype='float'
+    )
+    humidity_dataframe.index = pd.to_datetime(humidity_dataframe.index)
+
+    # Print that DataFrame to see what's inside
+    print('humidity_dataframe:')
+    print(humidity_dataframe, end='\n\n')
+
+    print('humidity_dataframe.index:')
+    print(humidity_dataframe.index, end='\n\n')
+
+    print('humidity_dataframe.describe():')
+    print(humidity_dataframe.describe(), end='\n\n')
+
+    print('humidity_dataframe.dtypes:')
+    print(humidity_dataframe.dtypes, end='\n\n')
